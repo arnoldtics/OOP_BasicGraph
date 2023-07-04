dc7ba02b1cbd542ba2d45f47539075a00a1ba28b
Arnoldo Sánchez <arfersan20@gmail.com>
2023-07-04 00:14:09 -0600

    Clase Grafo Dirigido

---
+++ claseGrafoDirigido.py
@@ -0,0 +1,42 @@
+class GrafoDirigido:
+    def __init__(self):
+        self.grafo = {}
+
+    def agregar_vertice(self, vertice):
+        if vertice not in self.grafo:
+            self.grafo[vertice] = {}
+
+    def agregar_arista(self, origen, destino, peso=1):
+        if origen in self.grafo and destino in self.grafo:
+            self.grafo[origen][destino] = peso
+
+    def obtener_vertices(self):
+        return list(self.grafo.keys())
+
+    def obtener_aristas(self):
+        aristas = []
+        for origen in self.grafo:
+            for destino in self.grafo[origen]:
+                aristas.append((origen, destino, self.grafo[origen][destino]))
+        return aristas
+
+    def obtener_vecinos(self, vertice):
+        if vertice in self.grafo:
+            return list(self.grafo[vertice].keys())
+        return []
+
+    def obtener_peso_arista(self, origen, destino):
+        if origen in self.grafo and destino in self.grafo[origen]:
+            return self.grafo[origen][destino]
+        return None
+
+    def eliminar_arista(self, origen, destino):
+        if origen in self.grafo and destino in self.grafo[origen]:
+            del self.grafo[origen][destino]
+
+    def eliminar_vertice(self, vertice):
+        if vertice in self.grafo:
+            del self.grafo[vertice]
+            for vertices in self.grafo.values():
+                if vertice in vertices:
+                    del vertices[vertice]
